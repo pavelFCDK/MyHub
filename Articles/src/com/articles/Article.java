6fe66fbeb78de1ed382955c1eb0e12107b715c96
Pavel Rabinovych <pavelrabinovych@gmail.com>
2014-12-11 11:07:28 +0200

    Articles
    Was added Article View Activity

--- Articles/src/com/articles/Article.java
+++ Articles/src/com/articles/Article.java
@@ -3,6 +3,9 @@ package com.articles;
 public class Article {
 	private static String id;
 	private static String title;
+	private static String author;
+	private static String content;
+	private static String subtilte;
 	
 	static public void setId(String i){
 		id = i;
@@ -16,5 +19,19 @@ public class Article {
 	}
 	static public String getTitle(){
 		return title;
+	}
+	
+	static public void setAuthor(String a){
+		author = a;
+	}
+	static public String getAuthor(){
+		return author;
+	}
+	
+	static public void setContent(String c){
+		content = c;
+	}
+	static public String getContent(){
+		return content;
 	}
 }
--- Articles/src/com/articles/ArticleViewActivity.java
+++ Articles/src/com/articles/ArticleViewActivity.java
@@ -1,6 +1,7 @@
 package com.articles;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,15 +10,19 @@ import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
@@ -35,6 +40,7 @@ public class ArticleViewActivity extends Activity {
 		setContentView(R.layout.activity_article_view);
 		
 		tv = (TextView)findViewById(R.id.textViewArticleContent);
+		tv.setText("");
 		
 		new ArticleView().execute();
 	}
@@ -61,16 +67,32 @@ public class ArticleViewActivity extends Activity {
 		protected String doInBackground(String... params) {
 			// TODO Auto-generated method stub
 			JSONParser jParser = new JSONParser();
-			//List<NameValuePair> param = new ArrayList<NameValuePair>();
-			//JSONArray json = jParser.getJSONFromUrl(ARTICLE_URL+Article.getId());
-			JSONObject json = jParser.getJSONOBJfromURL(ARTICLE_URL+Article.getId());
+			
+			List<NameValuePair> param = new ArrayList<NameValuePair>();
+			JSONObject json = jParser.makeHttpRequest(
+					ARTICLE_URL+Article.getId(), "GET", param);
+			System.out.println(ARTICLE_URL+Article.getId());
+			try {
+				String a = new String(json.getString("author").getBytes("ISO-8859-1"), "UTF-8");
+				Article.setAuthor(a);
+				String c = new String(json.getString("content").getBytes("ISO-8859-1"), "UTF-8");
+				Article.setContent(c);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return null;
 		}
 		
 		protected void onPostExecute(String file_url) {
             // dismiss the dialog once product deleted
             pDialog.dismiss();
-            tv.setText("12345");
+            tv.setText("Title: ".concat(Article.getTitle().concat("\n")));
+            tv.setText(tv.getText().toString().concat("Author: ".concat(Article.getAuthor().concat("\n"))));
+            tv.setText(tv.getText().toString().concat(Html.fromHtml(Article.getContent()).toString().concat("\n")));
 		}
 		
 	}
